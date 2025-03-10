b710e5caa4fadb9a293a0000fde384f9e63836f7
Mateus gabriel <senac.mateusgabrielferreira@gmil.com>
2025-03-10 20:03:37 -0300

    a

--- MultApps/MultApps.Windows/FrmCalculadoraIMC.cs
+++ MultApps/MultApps.Windows/FrmCalculadoraIMC.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MultiApps
+namespace MultApps.Windows
 {
     public partial class FrmCalculadoraIMC : Form
     {
@@ -17,29 +17,20 @@ namespace MultiApps
             InitializeComponent();
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void chkCrianca_CheckedChanged(object sender, EventArgs e)
         {
             chkCrianca.ForeColor = Color.DarkOrange;
-            cnkAdulto.ForeColor = Color.Gray;
-            cnkAdulto.Checked = false;
-
+            chkAdulto.ForeColor = Color.Gray;
+            chkAdulto.Checked = false;
+            lblIdade.Text = "Abaixo de 19 anos";
         }
 
-        private void cnkAdulto_CheckedChanged(object sender, EventArgs e)
+        private void chkAdulto_CheckedChanged(object sender, EventArgs e)
         {
-            cnkAdulto.ForeColor = Color.Gray;
-            chkCrianca.ForeColor = Color.DarkOrange;
+            chkAdulto.ForeColor = Color.DarkOrange;
+            chkCrianca.ForeColor = Color.Gray;
             chkCrianca.Checked = false;
+            lblIdade.Text = "Acima de 19 anos";
         }
     }
 }
