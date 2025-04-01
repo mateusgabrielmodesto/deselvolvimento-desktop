c9db3e94949ca0a50ed5b7a82fb1bc0f4f39691c
Mateus gabriel <senac.mateusgabrielferreira@gmil.com>
2025-04-01 19:22:07 -0300

    feat:Restruturação projeto

---
+++ MultiApps/MODEL/MultiApps.Models/Entidades/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiApps.Models.Entidades
+{
+    internal class Categoria
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public decimal Preco { get; set; }         
+        public int QuantidadeEmEstoque { get; set; }
+        public Categoria categoria { get; set; }
+    }
+}
